661318e7048c9c61e9e26b8126028156efd5c06b
Oussema3 <ldrajmioussema2@gmail.com>
2020-10-27 03:25:35 +0000

    HEADER

---
+++ holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif
