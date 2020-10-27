326fcaabdabd266aedcf3912f7e3072b22f3dcf1
Oussema3 <ldrajmioussema2@gmail.com>
2020-10-27 03:40:16 +0000

    try0

--- _printf.c
+++ _printf.c
@@ -6,7 +6,51 @@
  */
 int _printf(const char *format, ...)
 {
-  va_list rnd;
-  int i = 0, count = 0;
+  char *traverse; 
+  unsigned int i; 
+  char *s; 
+  va_list arg; 
+  va_start(arg, format); 
   
+  for(traverse = format; *traverse != '\0'; traverse++) 
+    { 
+      while( *traverse != '%' ) 
+	{ 
+	  putchar(*traverse);
+	  traverse++; 
+	} 
+      
+      traverse++; 
 
+      switch(*traverse) 
+	{ 
+	case 'c' : i = va_arg(arg,int);//Fetch char argument
+	  putchar(i);
+	  break; 
+	  
+	case 'd' : i = va_arg(arg,int); //Fetch Decimal/Integer argument
+	  if(i<0) 
+	    { 
+	      i = -i;
+	      putchar('-'); 
+	    } 
+	  puts(convert(i,10));
+	  break; 
+	  
+	case 'o': i = va_arg(arg,unsigned int); //Fetch Octal representation
+	  puts(convert(i,8));
+	  break; 
+	  
+	case 's': s = va_arg(arg,char *); //Fetch string
+	  puts(s); 
+	  break; 
+	  
+	case 'x': i = va_arg(arg,unsigned int); 
+	  puts(convert(i,16));
+	  break; 
+	}
+    } 
+  
+ 
+  va_end(arg); 
+} 
