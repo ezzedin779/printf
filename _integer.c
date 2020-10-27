8735250d785ac197d67170a100c9f261ae6b8843
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-27 10:43:16 +0100

    printing integers

--- _integer.c
+++ _integer.c
@@ -4,33 +4,33 @@
  *@spec: specifeirs
  *Return: int
  */
-   
-  int _integer(va_list spec)
-  {
-    int count = 0;
-    int abdesami3 = 1;
-    int a;
-    unsigned int b;
 
-    a = va_arg(spec, int);
-    if (a < 0)
-      {
-	_putchar ('-');
-	count++;
-	a *= -1;
-      }
-    b = a;
-    while ((a / abdesami3) >= 10)
-      {
-	abdesami3 *= 10;
-	b = a;
-      }
-    while (abdesami3 != 0)
-      {
-	count++;
-	_putchar ('0' + (b / abdesami3));
-	b = b % abdesami3;
-	abdesami3 = abdesami3 / 10;
-      }
-    return (count);
-  }
+int _integer(va_list spec)
+{
+int count = 0;
+int abdesami3 = 1;
+int a;
+unsigned int b;
+
+a = va_arg(spec, int);
+if (a < 0)
+{
+_putchar ('-');
+count++;
+a *= -1;
+}
+b = a;
+while ((a / abdesami3) >= 10)
+{
+abdesami3 *= 10;
+b = a;
+}
+while (abdesami3 != 0)
+{
+count++;
+_putchar ('0' + (b / abdesami3));
+b = b % abdesami3;
+abdesami3 = abdesami3 / 10;
+}
+return (count);
+}
