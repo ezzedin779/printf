36e140863fc6197902a7e4d8122b4cc46e9bbb49
Oussema3 <ldrajmioussema2@gmail.com>
2020-10-27 08:38:31 +0000

    convertion of 'i' and 'd'

---
+++ _integer.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * _integer - print integer
+ *@spec: specifeirs
+ *Return: int
+ */
+   
+  int _integer(va_list spec)
+  {
+    int count = 0;
+    int abdesami3 = 1;
+    int a;
+    unsigned int b;
+
+    a = va_arg(spec, int);
+    if (a < 0)
+      {
+	_putchar ('-');
+	count_int++;
+	a *= -1;
+      }
+    b = a;
+    while ((a / abdesami3) >= 10)
+      {
+	abdesami3 *= 10;
+	b = a;
+      }
+    while (abdesami3 != 0)
+      {
+	count_int++;
+	_putchar ('0' + (b / abdesami3));
+	b = b % abdesami3;
+	abdesami3 = abdesami3 / 10;
+      }
+    return (count);
+  }
