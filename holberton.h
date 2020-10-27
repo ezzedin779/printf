2f5a8f9d4f6acc163ba4e27a1329d40ee893b08c
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-27 10:37:19 +0100

    HEADER 0.2

--- holberton.h
+++ holberton.h
@@ -6,9 +6,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void print_string(char *str);
+/**
+*struct choose_right - choosing the right type
+*@type: the type to print
+*@func: func of the type to print
+*/
+typedef struct choose_right
+{
+	char type;
+	int (*func)(va_list arg);
+} choose_t;
+
+int _integer(va_list spec);
+int checker_spec(char c, va_list liste);
+int _print_char(va_list spec);
+int _percentage(__attribute__((unused))va_list spec);
+int _print_string(va_list spec);
 int _putchar(char c);
 int _printf(const char *format, ...);
-
-
 #endif
