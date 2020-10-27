32ac6d2607b871fe5781badaccb5d1c6dc09d839
ezzedin779 <abdou.babchia64@gmail.com>
2020-10-27 10:44:18 +0100

    printing string

--- _print_string.c
+++ _print_string.c
@@ -2,7 +2,7 @@
 /**
  * _print_string - Prints a string.
  * @spec: specifiers list.
- * 
+ *
  */
 int _print_string(va_list spec)
 {
@@ -14,5 +14,5 @@ if (pnt == NULL)
 pnt = "(null)";
 for (i = 0; pnt[i] != '\0'; i++)
 _putchar(pnt[i]);
-return (i);    
-  }
+return (i);
+}
