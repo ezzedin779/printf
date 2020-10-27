cd8ffc7853c284cd5b6fce2e6f055b7f4f281916
Oussema3 <ldrajmioussema2@gmail.com>
2020-10-27 06:14:12 +0000

    creating printf

--- _printf.c
+++ _printf.c
@@ -1,4 +1,6 @@
+ 
 #include "holberton.h"
+void print_string(char *str);
 /**
  *_printf - print output considering the format.
  *@format: format of output
@@ -6,51 +8,53 @@
  */
 int _printf(const char *format, ...)
 {
-  char *traverse; 
+  char const *traverse; 
   unsigned int i; 
   char *s; 
-  va_list arg; 
+  va_list arg;
+  int count = 0;
+
   va_start(arg, format); 
   
-  for(traverse = format; *traverse != '\0'; traverse++) 
-    { 
-      while( *traverse != '%' ) 
-	{ 
-	  putchar(*traverse);
-	  traverse++; 
-	} 
-      
-      traverse++; 
-
-      switch(*traverse) 
-	{ 
-	case 'c' : i = va_arg(arg,int);//Fetch char argument
-	  putchar(i);
-	  break; 
-	  
-	case 'd' : i = va_arg(arg,int); //Fetch Decimal/Integer argument
-	  if(i<0) 
+  for (traverse = format; *traverse != '\0'; traverse++) 
+    {
+      while (*traverse != '\0')
+	{      while ( *traverse != '%') 
 	    { 
-	      i = -i;
-	      putchar('-'); 
-	    } 
-	  puts(convert(i,10));
-	  break; 
-	  
-	case 'o': i = va_arg(arg,unsigned int); //Fetch Octal representation
-	  puts(convert(i,8));
-	  break; 
-	  
-	case 's': s = va_arg(arg,char *); //Fetch string
-	  puts(s); 
-	  break; 
-	  
-	case 'x': i = va_arg(arg,unsigned int); 
-	  puts(convert(i,16));
-	  break; 
+	      _putchar(*traverse);
+	      traverse++;
+	      count++;
+	    }
+	  traverse++; 
+	  switch(*traverse) 
+	    { 
+	    case 'c' : i = va_arg(arg,int);
+	      _putchar(i);
+	      count++;
+	      break; 
+
+	    case 's': s = va_arg(arg,char *); 
+	      print_string(s);
+	      count++; 
+	      break; 
+	    case '%': _putchar('%');
+	      count++;
+	      break;
+	    }
 	}
-    } 
-  
- 
-  va_end(arg); 
-} 
+    }
+  va_end(arg);
+  return (count);
+}
+/**
+ *print_string - printing a string
+ *@str: string
+ */
+void print_string(char *str)
+{
+  int i;
+
+  for (i = 0; str[i] != '\0'; i++)
+    _putchar(str[i]);
+  str[i] = '\0';
+}
